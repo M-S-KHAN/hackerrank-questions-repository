13f4cc072df4ea29716ddb0d3827e5351c61fd5f
M-S-KHAN <sajawalkhan111@gmail.com>
2022-08-30 21:53:52 +0500

    - Added Merge the Tools

---
+++ Merge the Tools - Medium/solution.py
@@ -0,0 +1,13 @@
+def merge_the_tools(string, k):
+    if k == 1:
+        for i in range(len(string)):
+            print(string[i])        
+        return
+    
+    for i in range(k):
+        substr = string[i*k:(i*k)+k]
+        vals = ""
+        for i in range(len(substr)):
+            if substr[i] not in vals:
+                vals += substr[i]
+        print(vals)
