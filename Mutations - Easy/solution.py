42ae5bf41f204d619c75af46b5c77915d6cee948
M-S-KHAN <sajawalkhan111@gmail.com>
2022-08-31 21:42:18 +0500

    - Added Mutations

---
+++ Mutations - Easy/solution.py
@@ -0,0 +1,14 @@
+def mutate_string(string, position, character):
+    chars = []
+    is_inserted = False
+    for i in range(len(string)+1):
+        if (i > len(string) - 1):
+            break
+        if i == position:
+            chars.append(character)
+            is_inserted = True
+        else:
+            chars.append(string[i])
+
+    
+    return ''.join(chars)
