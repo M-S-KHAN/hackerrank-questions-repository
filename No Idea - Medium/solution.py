8b336c92c570a6cc3071e9b452e0429856862e3c
M-S-KHAN <sajawalkhan111@gmail.com>
2022-09-02 11:34:23 +0500

    - Added No Idea

---
+++ No Idea - Medium/solution.py
@@ -0,0 +1,14 @@
+n, m = [int(i) for i in input().split()]
+arr = [int(i) for i in input().split()]
+A = set([int(i) for i in input().split()])
+B = set([int(i) for i in input().split()])
+
+total = 0
+
+for i in range(len(arr)):
+    if arr[i] in A:
+        total += 1
+    if arr[i] in B:
+        total -= 1
+        
+print(total)
